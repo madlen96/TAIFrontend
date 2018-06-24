4d05870e8f5c8ea0c41d5749c5d6e2666e7bf73c
madlen96 <magdalenka.k96@gmail.com>
2018-06-24 11:56:44 +0200

    edit version of app

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import { ImplicitCallback, Security } from '@okta/okta-react';
 
 const config = {
   issuer: 'https://dev-380192.oktapreview.com/oauth2/default',
-  redirectUri: 'https://dev-380192.oktapreview.com/login/login.htm?fromURI=/oauth2/v1/authorize/redirect?okta_key=QxU8Y_jfv1t1q_GE4-VSH6RP0rnIPfmhnWIDPHGnrrU',
-  // window.location.origin + '/implicit/callback',
+  redirectUri: window.location.origin + '/implicit/callback',
   clientId: '0oaf4lvtrs4NfXz330h7'
 };
 
